47af3aabcb1b6fbdd3b6dcf25230e25cab6bbe80
MolecularDeveloper <moleculardeveloper@gmail.com>
2018-07-11 14:38:52 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+        <div class="columns is-fullheight">
+      <div class="column is-2 is-sidebar-menu">
+        <aside class="menu">
+          <p class="menu-label">
+            General
+          </p>
+          <ul class="menu-list">
+            <router-link tag="li" to="/"><a>Home</a></router-link>
+             <router-link tag="li" to="/history"><a>History</a></router-link>
+          </ul>
+        </aside>
+      </div>
+      <div class="column is-main-content">
+
+            <transition name="slide-fade"><router-view /></transition>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.slide-fade-enter-active {
+  transition: all .8s ease;
+}
+
+.slide-fade-enter
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(1000px);
+  opacity: 0;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="hello">
+   <p>{{location}}</p>
+    <section class="section">
+      <div class="container">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">
+              {{book.title}} by:<span class="author">&nbsp;{{book.author}}</span>
+            </p>
+            <p class="card-header-icon">
+              Position:&nbsp;{{book.points}}
+            </p>
+          </header>
+          <div class="card-content">
+            Description... <a href="#" aria-label="more options" v-on:click="toggleDescription">
+      <span class="icon">
+        <i class="fa fa-angle-down"></i>
+      </span>
+    </a>
+
+            <div v-show="isToggled" class="content description">
+            <div class="book-img">
+              <img :src="book.img" />
+            </div>
+            <div class="book-description">
+              <p>
+              {{book.description}}
+              </p>
+            </div>
+          </div>
+          </div>
+          <div style="clear:both;"></div>
+          <footer class="card-footer">
+            <a href="#" class="card-footer-item">Read More</a>
+            <a href="#" class="card-footer-item" v-on:click="downVote">Down Vote  <br/><i class="fa fa-arrow-down"></i></a>
+            <a href="#" class="card-footer-item" v-on:click="upVote">Up Vote  <br/><i class="fa fa-arrow-up"></i></a>
+          </footer>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      location: 'Home',
+      book: {
+        title: '12 Rules for Life: An Antidote to Chaos',
+        author: 'Jordan Peterson',
+        description: "Humorous, surprising and informative, Dr. Peterson tells us why skateboarding boys and girls must be left alone, what terrible fate awaits those who criticize too easily, and why you should always pet a cat when you meet one on the street. What does the nervous system of the lowly lobster have to tell us about standing up straight (with our shoulders back) and about success in life? Why did ancient Egyptians worship the capacity to pay careful attention as the highest of gods? What dreadful paths do people tread when they become resentful, arrogant and vengeful? Dr. Peterson journeys broadly, discussing discipline, freedom, adventure and responsibility, distilling the world's wisdom into 12 practical and profound rules for life. 12 Rules for Life shatters the modern commonplaces of science, faith and human nature, while transforming and ennobling the mind and spirit of its readers.'",
+        level: 1,
+        points: 0,
+        voted: false,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/41Z06PJRlZL.jpg'
+      },
+      isToggled: false
+    }
+  },
+  methods: {
+    upVote: function () {
+      this.book.points++
+    },
+    downVote: function () {
+      this.book.points--
+    },
+    toggleDescription: function () {
+      this.isToggled = !this.isToggled
+    }
+  }
+}
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+section.nav-section {
+  width: 300px;
+  height: 100vh;
+}
+
+.author {
+  font-style: italic;
+}
+
+.description {
+  font-size: 12px;
+}
+.book-img {
+  max-width: 100px;
+  float:left;
+  padding: 0 10px;
+}
+.book-description {
+}
+.card-footer {
+  margin-top: 20px;
+}
+
+</style>
